af9e1a6957e407f4e5c295c05a3887a9ca796c13
iagosm <iagosousa201486@gmail.com>
2023-06-18 20:59:44 -0300

    iniciando projeto to-do com php

---
+++ index.php
@@ -0,0 +1,51 @@
+
+<?php 
+require 'db_conn.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>To-do</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="main-section">
+    <div class="add-section">
+      <form action="" method="POST" autocomplete="off">
+        <input type="text" name="title" placeholder="Esse campo é necessário">
+        <button type="submit">Add &nbsp; <span>&#43;</span></button>
+      </form>
+    </div>
+    <?php 
+    $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+
+    ?>
+    <div class="show-todo-section">
+      <?php if($todos->rowCount() === 0) { ?>
+      <div class="todo-item">
+        <div class="empty">
+          <img src="assets/todo.png" alt="">
+        </div>
+      </div>
+      <?php } ?>
+      <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) {?>
+      <div class="todo-item">
+        <span id="<?php echo $todo['id'];?>" class="remove-to-do">x</span>
+        <?php if($todo['checked']) { ?>
+          <input type="checkbox" class="check-box" checked>
+          <h2 class="checked"><?php echo $todo['title']?></h2>
+        <?php }else { ?>
+          <input type="checkbox" class="check-box">
+          <h2><?php echo $todo['title']?></h2>
+        <?php } ?>
+        <br>
+        <small>Created: <?php echo $todo['date_time']?></small>
+      </div>
+      <?php }?>
+    </div>
+  </div>
+  
+</body>
+</html>
