5846fb30fa00724b4eadcab8a0883a7a9ec0811e
iagosm <iagosousa201486@gmail.com>
2023-06-18 23:26:55 -0300

    finalizando o projeto todo

--- index.php
+++ index.php
@@ -13,9 +13,14 @@ require 'db_conn.php';
 <body>
   <div class="main-section">
     <div class="add-section">
-      <form action="" method="POST" autocomplete="off">
-        <input type="text" name="title" placeholder="Esse campo é necessário">
+      <form action="app/add.php" method="POST" autocomplete="off">
+        <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error'){ ?>
+          <input type="text" name="title" placeholder="Esse campo é necessário" style="border-color: #ff6666">
+          <button type="submit">Add &nbsp; <span>&#43;</span></button>
+        <?php }else {?>
+        <input type="text" name="title" placeholder="O que você precisa fazer?">
         <button type="submit">Add &nbsp; <span>&#43;</span></button>
+        <?php } ?>
       </form>
     </div>
     <?php 
@@ -34,10 +39,10 @@ require 'db_conn.php';
       <div class="todo-item">
         <span id="<?php echo $todo['id'];?>" class="remove-to-do">x</span>
         <?php if($todo['checked']) { ?>
-          <input type="checkbox" class="check-box" checked>
+          <input type="checkbox" data-todo-id = "<?php echo $todo['id'];?>" class="check-box" checked>
           <h2 class="checked"><?php echo $todo['title']?></h2>
         <?php }else { ?>
-          <input type="checkbox" class="check-box">
+          <input type="checkbox" data-todo-id = "<?php echo $todo['id'];?>" class="check-box">
           <h2><?php echo $todo['title']?></h2>
         <?php } ?>
         <br>
@@ -46,6 +51,45 @@ require 'db_conn.php';
       <?php }?>
     </div>
   </div>
-  
+  <script src="js/jquery.js"></script>
+  <script>
+    $(document).ready(function(){
+      $('.remove-to-do').click(function(){
+        const id = $(this).attr('id');
+        
+        $.post("app/remove.php", 
+        {
+          id: id
+        },
+        (data)  => {
+          if(data) {
+            $(this).parent().hide(600);
+          }
+        }
+      )
+    });
+
+    $(".check-box").click(function(e){
+      const id = $(this).attr('data-todo-id');
+      
+      $.post('app/check.php',
+      {
+        id: id
+      },
+      (data) => {
+        if(data != 'error') {
+          const h2 = $(this).next();
+          if(data === '1') {
+            h2.removeClass('checked');
+          }else {
+            h2.addClass('checked');
+          }
+        }
+      }
+      )
+    })
+  });
+    
+  </script>
 </body>
 </html>
